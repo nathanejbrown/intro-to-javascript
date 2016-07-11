72eff05a4e339c2f7b839ac28791e3f098380ba8
Heathcliff <dennis.nathan@gmail.com>
2016-07-11 11:41:44 -0600

    added intro-to-programming file, with solutions to all problems.

---
+++ intro-to-programming.js
@@ -0,0 +1,70 @@
+//Problems
+//1:
+for(x=0; x<=1000; x+=100) {
+  console.log(x);
+}
+//2:
+for(x=1; x<129; x*=2) {
+  console.log(x);
+}
+//3:
+for(x=0; x<11; x+=2) {
+  console.log(x);
+}
+//4:
+for(x=3; x<16; x+=3) {
+  console.log(x);
+}
+//5:
+for(x=9; x>=0; x--) {
+  console.log(x);
+}
+//6:
+for(x=1; x<5; x++) {
+  for(y=0; y<3; y++) {
+  console.log(x);
+  }
+}
+//7:
+for(x=0; x<3; x++) {
+  for(y=0; y<5; y++)
+  console.log(y);
+}
+
+//"Write a program..."
+//1-25 even or not
+for(x=1; x<26; x++) {
+  if(x % 2 === 0) {
+    console.log(x + " is even");
+  }
+  else console.log(x + " is odd");
+}
+//fizzbuzz:
+for(x=1; x<51; x++) {
+  if(x%15 === 0) {
+    console.log("fizzbuzz");
+  }
+  else if(x%3 === 0) {
+    console.log("fizz");
+  }
+  else if(x%5 === 0) {
+    console.log("buzz");
+  }
+  else {
+    console.log(x);
+  }
+}
+//multiples of 3 and 5 under 1000
+count = 0;
+for(x=0; x<=1000; x++) {
+  if(x%3 === 0 || x%5 === 0) {
+    count += x;
+  }
+}
+console.log("The sum is: " + count);
+//triangle
+var hash = "#";
+for(x=1; x<8; x++) {
+  console.log(hash);
+  hash += "#";
+}
