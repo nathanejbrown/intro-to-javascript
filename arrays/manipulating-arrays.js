ce2c5dd474ffdcca55cc649c323c51f1c1f0f384
Heathcliff <dennis.nathan@gmail.com>
2016-07-12 13:38:49 -0600

    Corrected confusing syntax in manipulating-arrays.js

--- arrays/manipulating-arrays.js
+++ arrays/manipulating-arrays.js
@@ -1,12 +1,14 @@
+//take an input array of [[11,13],[0,2]] and return //[2,0,13,11]
+
 input = [[11,13],[0,2]];
 
-function arrayThing(input) {
-  input = input.reverse();
-  for(x=0; x<(input.length-1); x++) {
-    input[x] = input[x].reverse();
+function arrayThing(n) {
+  n = n.reverse();
+  for(x=0; x<(n.length-1); x++) {
+    n[x] = n[x].reverse();
   }
-  input = input[0].concat(input[1]);
-  return input;
+  n = n[0].concat(n[1]);
+  return n;
 }
 
 console.log(arrayThing(input));
